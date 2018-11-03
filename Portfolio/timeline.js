6e51debeff8e67df224e626636383267f5f75f10
jtb3607 <jtb3607@IGM2000-26>
2018-11-03 16:04:55 -0400

    trying to get a javascript click event  working

--- Portfolio/timeline.js
+++ Portfolio/timeline.js
@@ -1,9 +1,29 @@
-window.onload=function()
-{
-var photo = document.querySelectorAll(".photoContainer");
-photo.onClick = function(e)
-{
-    var yPosition = event.clientY - photo.getBoundingClientRect().top - (photo.clientHeight / 2);
-    photo.style.top = yPosition + "px";
-}
-}
+// window.onload=function()
+// {
+// var photo = document.querySelectorAll(".photoContainer");
+// photo.onClick = function(e)
+// {
+//     var yPosition = event.clientY - photo.getBoundingClientRect().top - (photo.clientHeight / 2);
+//     photo.style.top = yPosition + "px";
+// }
+// }
+
+// var containter = $('main').wrapAll('<div />');
+
+// containter.css('position','relative');
+
+// containter.toggle(
+//     function(){
+//         $(this).animate({top: '-20px'}, 500);
+//     }
+    // },function() {
+    //     $(this).animate({}, 500);
+    //  }
+// );
+
+// var container = document.querySelector("photoContainer");
+
+// container.click(function(){
+//     var i = $(this).position.top();
+//     $(this).position.top = i + 200;
+// })
