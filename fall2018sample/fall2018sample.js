66df45af7363bd107fbdbfdcc8e6eb7655b42f0e
Joshua Bullock (RIT Student) <jtb3607@ad.rit.edu>
2018-12-04 21:53:11 -0500

    almost finished practical

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,1 +1,14 @@
 /* Don't use <script> tags in a linked js file! */
+let content = "content1.txt";
+$("#content").load(content);
+
+$("#choose-content").change(function() {
+    content = $(this).val();
+    $("#content").load(content);
+});
+
+$(".menuitem").click(function() {
+    $(this).next(".submenu").slideToggle();
+});
+
+
